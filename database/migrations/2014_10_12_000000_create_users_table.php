6bfc4780628e7a69354dbb2e4cd58470ea739193
Hafiz Hadi <hafizhadi581@gmail.com>
2024-01-04 11:32:37 +0800

    changelog: fix change id to ulid

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
         try
         {
             Schema::create('users', function (Blueprint $table) {
-                $table -> id();
-                $table -> string('name');
+                $table -> ulid('id') -> primary();
+                $table -> string('name', 50);
                 $table -> string('email') -> unique();
                 $table -> timestamp('email_verified_at') -> nullable();
                 $table -> string('password');
