1eedc69534a25a98902eb595497a06fe2f3c683e
Hafiz Hadi <hafizhadi581@gmail.com>
2024-01-04 16:42:04 +0800

    add district malaysia migration

---
+++ database/migrations/2024_01_04_075952_create_districts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        try
+        {
+            Schema::create('districts', function (Blueprint $table) {
+                $table -> ulid('id') -> primary();
+                $table -> string('name', 40) -> nullable();
+                $table -> string('district_code', 4) -> nullable();
+
+                // Track
+                $table -> softDeletes();
+                $table -> timestamps();
+            });
+        }
+        catch (\Throwable)
+        {
+            Schema::dropIfExists('districts');
+        }
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('districts');
+    }
+};
