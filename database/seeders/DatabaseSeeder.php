15312660e15ac55659ea6ae70d8ec5213cf563cc
Hafiz Hadi <hafizhadi581@gmail.com>
2024-01-04 17:36:04 +0800

    add seeder state and district seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,15 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\DistrictSeeder;
+use Database\Seeders\Locations\StateSeeder;
+use Database\Seeders\Locations\DistrictSeeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         $this -> call([
+            StateSeeder::class,
             DistrictSeeder::class,
         ]);
     }
